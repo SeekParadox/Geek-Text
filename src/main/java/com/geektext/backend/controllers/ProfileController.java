737255fecc736f94a566edbcce2c52bbdf60140f
David Ulloa <dulloa6310@gmail.com>
2023-07-01 17:31:42 -0700

    Finished features 3 and 4: update user and add credit card

--- src/main/java/com/geektext/backend/controllers/ProfileController.java
+++ src/main/java/com/geektext/backend/controllers/ProfileController.java
@@ -1,6 +1,6 @@
 package com.geektext.backend.controllers;
 
-import com.geektext.backend.UserDataParser;
+import com.geektext.backend.models.CreditCard;
 import com.geektext.backend.dao.UserService;;
 import com.geektext.backend.models.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,27 +19,27 @@ public class ProfileController {
 
     @ResponseBody
     @GetMapping("/user")
-    public ResponseEntity<UserEntity> getUser(@RequestBody UserDataParser jsonUser) {
+    public ResponseEntity<UserEntity> getUser(@RequestBody UserEntity jsonUser) {
         String username = jsonUser.getUsername();
         if (username.isEmpty()) {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
 
-        if (!userService.findUserByUsername(jsonUser.getUsername())) {
+        if (!userService.doesUserExist(jsonUser.getUsername())) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
 
-        UserEntity user = userService.findAllUsers(username).get(0);
+        UserEntity user = userService.getUser(username);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @ResponseBody
     @PostMapping("/user")
-    public ResponseEntity<String> createUser(@RequestBody UserDataParser jsonUser) {
+    public ResponseEntity<String> createUser(@RequestBody UserEntity jsonUser) {
         if (jsonUser.getUsername() == null || jsonUser.getPassword() == null)
             return new ResponseEntity<>("Username or Password were not passed into body", HttpStatus.BAD_REQUEST);
 
-        if (userService.findUserByUsername(jsonUser.getUsername())) {
+        if (userService.doesUserExist(jsonUser.getUsername())) {
             return new ResponseEntity<>("User (unique by username) already exists", HttpStatus.BAD_REQUEST);
         }
 
@@ -49,6 +49,36 @@ public class ProfileController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @PutMapping("/user")
+    public ResponseEntity<UserEntity> updateUser(@RequestBody UserEntity jsonUser) {
+        if (!userService.doesUserExist(jsonUser.getUsername())) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            UserEntity updatedUser = userService.updateUser(jsonUser.getUsername(), jsonUser);
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @ResponseBody
+    @PostMapping("/creditCard")
+    public ResponseEntity<String> createCreditCard(@RequestBody CreditCard jsonCreditCard) {
+        if (!CreditCard.isValid(jsonCreditCard)) {
+            return new ResponseEntity<>("You must fill out each field of the credit card object", HttpStatus.BAD_REQUEST);
+        }
+
+        if (!userService.doesUserExist(jsonCreditCard.getUsername())) {
+            return new ResponseEntity<>("User does not exist", HttpStatus.NOT_FOUND);
+        }
+
+        userService.addCreditCard(jsonCreditCard.getUsername(), jsonCreditCard);
         return new ResponseEntity<>("success", HttpStatus.OK);
     }
 }
